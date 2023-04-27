1d0405c0bea033574a4659e179497a6fddcaf405
osamaSale <osama.mohamad.salem@gmail.com>
2023-04-27 20:57:36 +0300

    title of the changes you did

--- api/users/delete.php
+++ api/users/delete.php
@@ -4,18 +4,18 @@ error_reporting(0);
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: DELETE");
-header('Access-Control-Allow-Headers: Content-Type , Access-Control-Allow-Headers , Authorization,X-Request-with');
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 include("./function.php");
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
 if ($requestMethod === "DELETE") {
     $deleteUser = deleteUser($_GET);
     echo $deleteUser;
-} else {
+}/*  else {
     $data = [
         "status" => 405,
         "massage" => $requestMethod . 'Method Not Allowed'
     ];
     header("HTTP/1.0 405 Method Not Allowed");
     echo json_encode($data);
-}
+} */
